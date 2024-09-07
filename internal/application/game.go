05b4e60ab210b7da4220485161ed6af2e6af040a
Maksim Klychkov <makly@MacBook-Pro-Maksim.local>
2024-09-07 17:17:46 +0300

    base game struct

---
+++ internal/application/game.go
@@ -0,0 +1,39 @@
+package application
+
+type Game struct {
+	attempts int
+	mistakes int
+	word     string
+	guessed  map[rune]bool
+	used     map[rune]bool
+}
+
+func NewGame(word string) *Game {
+	return &Game{
+		attempts: 0,
+		mistakes: 0,
+		word:     word,
+		guessed:  make(map[rune]bool),
+		used:     make(map[rune]bool),
+	}
+}
+
+func (g *Game) GetAttempts() int {
+	return g.attempts
+}
+
+func (g *Game) GetMistakes() int {
+	return g.mistakes
+}
+
+func (g *Game) GetPattern() string {
+	pattern := ""
+	for _, letter := range g.word {
+		if g.guessed[letter] {
+			pattern += string(letter)
+		} else {
+			pattern += "_"
+		}
+	}
+	return pattern
+}
