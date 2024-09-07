96ded40c48e300cba635ef2b0f85aa0e6fbb7bec
Maksim Klychkov <makly@MacBook-Pro-Maksim.local>
2024-09-07 19:50:01 +0300

    added logic to game.go

--- internal/domain/game.go
+++ internal/domain/game.go
@@ -1,20 +1,25 @@
 package domain
 
 type Game struct {
-	attempts int
-	mistakes int
-	word     string
-	guessed  map[rune]bool
-	used     map[rune]bool
+	attempts       int
+	mistakes       int
+	word           string
+	correctLetters map[rune]bool
+	used           map[rune]bool
 }
 
 func NewGame(word string) *Game {
+	correctLetters := make(map[rune]bool)
+	for _, letter := range word {
+		correctLetters[letter] = true
+	}
+
 	return &Game{
-		attempts: 0,
-		mistakes: 0,
-		word:     word,
-		guessed:  make(map[rune]bool),
-		used:     make(map[rune]bool),
+		attempts:       0,
+		mistakes:       0,
+		word:           word,
+		correctLetters: correctLetters,
+		used:           make(map[rune]bool),
 	}
 }
 
@@ -29,7 +34,7 @@ func (g *Game) GetMistakes() int {
 func (g *Game) GetPattern() string {
 	pattern := ""
 	for _, letter := range g.word {
-		if g.guessed[letter] {
+		if g.used[letter] && g.correctLetters[letter] {
 			pattern += string(letter)
 		} else {
 			pattern += "_"
@@ -38,10 +43,33 @@ func (g *Game) GetPattern() string {
 	return pattern
 }
 
-func (g *Game) Guess(letter rune) bool {
-	return false
+func (g *Game) Guess(letter rune) {
+	if g.used[letter] {
+		return
+	}
+
+	g.attempts++
+	g.used[letter] = true
+	if g.correctLetters[letter] {
+		return
+	}
+
+	g.mistakes++
+}
+
+func (g *Game) IsWin() bool {
+	for _, letter := range g.word {
+		if !g.used[letter] {
+			return false
+		}
+	}
+	return true
+}
+
+func (g *Game) IsLose() bool {
+	return g.mistakes >= 6
 }
 
 func (g *Game) IsFinished() bool {
-	return false
+	return g.IsLose() || g.IsWin()
 }
