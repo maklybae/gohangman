4824c3ea7b8d7eab4fa1813a9b4ea04869179f68
Maksim Klychkov <makly@MacBook-Pro-Maksim.local>
2024-09-08 17:01:02 +0300

    console input get letter method

--- internal/infrastructure/console_input.go
+++ internal/infrastructure/console_input.go
@@ -1,1 +1,14 @@
 package infrastructure
+
+import (
+	"fmt"
+)
+
+type ConsoleInput struct {
+}
+
+func (c *ConsoleInput) GetLetter() rune {
+	var letter rune
+	fmt.Scanf("%c\n", &letter)
+	return letter
+}
