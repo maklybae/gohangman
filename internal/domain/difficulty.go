ac15ce8f36a44a6e3c5d1d6be048fe7759797e1b
Maksim Klychkov <makly@MacBook-Pro-Maksim.local>
2024-09-14 16:47:50 +0300

    difficulties changed

--- internal/domain/difficulty.go
+++ internal/domain/difficulty.go
@@ -6,4 +6,21 @@ const (
 	EasyDifficulty Difficulty = iota
 	MediumDifficulty
 	HardDifficulty
+	UnknownDifficulty
 )
+
+func (d Difficulty) String() string {
+	return [...]string{"Easy", "Medium", "Hard"}[d]
+}
+
+func (d Difficulty) Set(value string) error {
+	switch value {
+	case "easy":
+		d = EasyDifficulty
+	case "medium":
+		d = MediumDifficulty
+	case "hard":
+		d = HardDifficulty
+	}
+	return nil
+}
