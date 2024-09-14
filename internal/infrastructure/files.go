1b7192bfce2f2f258442b31e38a0d26aca566ef0
Maksim Klychkov <makly@MacBook-Pro-Maksim.local>
2024-09-14 17:23:17 +0300

    errors in file reading

--- internal/infrastructure/files.go
+++ internal/infrastructure/files.go
@@ -2,29 +2,38 @@ package infrastructure
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io"
 	"makly/hangman/internal/domain"
 	"os"
 )
 
-func ReadCollectionFromFile(path string) (*domain.WordsCollection, error) {
-	wordsCollection := domain.WordsCollection{}
-
+func ReadCollectionFromFile(path string) (wordsCollection *domain.WordsCollection, err error) {
 	file, err := os.Open(path)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("open file: %w", err)
 	}
-	defer file.Close()
+
+	defer func() {
+		if closeErr := file.Close(); closeErr != nil {
+			if err != nil {
+				err = errors.Join(err, closeErr)
+				return
+			}
+			err = fmt.Errorf("close file: %w", closeErr)
+		}
+	}()
 
 	byteValue, err := io.ReadAll(file)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("read file: %w", err)
 	}
 
 	err = json.Unmarshal(byteValue, &wordsCollection)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("unmarshal json: %w", err)
 	}
 
-	return &wordsCollection, nil
+	return wordsCollection, nil
 }
